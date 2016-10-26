147e7d71195e10a3b77c752648889e604ad33a11
Sborzguilherme <sborzguilherme@gmail.com>
2016-10-26 15:48:26 -0200

    Sem Spinner

--- app/src/main/java/com/example/guilhermesborz/addcontato/MainActivity.java
+++ app/src/main/java/com/example/guilhermesborz/addcontato/MainActivity.java
@@ -3,31 +3,43 @@ package com.example.guilhermesborz.addcontato;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import fr.ganfra.materialspinner.MaterialSpinner;
 
+
 public class MainActivity extends AppCompatActivity {
 
     private  EditText etNome;
-    //private Button btMaisCampos;
-    //private Button btSalvar;
+    //Spinner spinner1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Toast.makeText(this.getApplicationContext(), "Mensagem", Toast.LENGTH_SHORT).show();
-
         etNome =(EditText) findViewById(R.id.etNome);
-        //btMaisCampos = (Button)findViewById(R.id.btMaisCampos);
-        //btSalvar = (Button)
+        //spinner1= (Spinner)findViewById(R.id.spinner1);
+
+        ArrayAdapter adapter = ArrayAdapter.createFromResource(this, R.array.Usuarios_email, R.layout.support_simple_spinner_dropdown_item);
+        //spinner1.setAdapter(adapter);
+        //spinner1.setOnItemSelectedListener((AdapterView.OnItemSelectedListener) this);
 
     }
+
+
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        TextView myText = (TextView) view;
+        Toast.makeText(this, "Você escolheu" + myText.getText(), Toast.LENGTH_LONG);
+    }
+
+
 
     public void clicouNoBotao(View origemDoClique){
 
@@ -35,10 +47,6 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    String[] ITEMS = {"Item 1", "Item 2", "Item 3", "Item 4", "Item 5", "Item 6"};
-    ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, ITEMS);
-    adapter.setDropDownViewResource();
-    MaterialSpinner spinner = (MaterialSpinner) findViewById(R.id.spinner);
-    MaterialSpinner.setAdapter(adapter);
+
 
 }
